c14178a9dd704ffa52dad3beb615583b597fd53f
skanejohan <johan_m_ahlgren@hotmail.com>
2018-05-30 21:46:21 +0200

    Parswick game updated

--- Theseus/Parswick/build.bat
+++ Theseus/Parswick/build.bat
@@ -1,1 +1,4 @@
 D:\Tools\Theseus\Theseus.exe parswick.theseus
+copy game\parswick.js "..\..\Web apps\parswick\parswick.game.js"
+copy game\parswick.framework.js "..\..\Web apps\parswick\theseus.framework.js"
+
