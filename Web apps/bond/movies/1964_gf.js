409c6bcac582a4442db2e06c01ba8d4a976f4342
skanejohan <johan_m_ahlgren@hotmail.com>
2019-01-30 20:05:09 +0100

    Bond: Goldfinger added

---
+++ Web apps/bond/movies/1964_gf.js
@@ -0,0 +1,297 @@
+const gf = {
+    name: "Goldfinger",
+    year: 1964,
+    scenes : [
+        {
+            // 00:00:00
+            title: "Gun-barrel sequence",
+            description: "For the last time performed by stunt man Bob Simmons. Doesn't open up as so many have done after it, but the gun-barrel disappears before the whole screen is taken up by...",
+        },
+        {
+            // 00:00:35
+            title: "Heroin-flavoured bananas",
+            description: "...the pre-title sequence, this time recorded at an oil storage depot in [Stanwell|stanwell], just south of Heathrow Airport, doubling for an unknown Latin American country. After blowing up the heroin, Bond \"kills\" stunt man Alf Joint - normally Connery's own stunt double - who took on the role of Capungo with short notice.",
+        },
+        {
+            // 00:04:55
+            title: "Theme song",
+            description: "Maybe the most iconic of them all, lovely sung by Shirley Bassey. The title sequence itself is designed by Robert Brownjohn.",
+        },
+        {
+            // 00:07:35
+            title: "\"Welcome to Miami Beach\"",
+            description: "We meet Bond as he is on vacation in Miami. He is staying at the [Fountainbleau Hotel|fountainbleau], although in reality, Connery and Fröbe were never on location - their scenes were all shot at Pinewood. Here, we get another iconic scene - a dead Jill Masterson covered in gold paint.",
+        },
+        {
+            // 00:17:10
+            title: "London",
+            description: "After an air view of the [Houses of Parliament|hp], we see a rather subdued Bond being told off by M, not at all impressed with the Miami business. Later the same evening, Bond is assigned to investigate Mr. Goldfinger, who MI6 suspects of smuggling gold.",
+        },
+        {
+            // 00:21:45
+            title: "Q branch",
+            description: "For the first time, we see Q branch, and we meet Q himself, played for the second time by Desmond Llewellyn. On M's orders, Bond's trusted Bentley is now replaced with the Aston Martin DB5 that we will see in several subsequent films.",
+        },
+        {
+            // 00:24:20
+            title: "A game of golf",
+            description: "The golf game between Bond and Goldfinger was recorded at [Stoke Park|stoke]. The golf shop, where Bond and Goldfinger are introduced was located [in front of the clubhouse|golfshop], but has since been removed. The [green of the 18th hole|hole18], where the duel between Bond and Goldfinger culminates, is still located just behind the clubhouse. The shot where the statue so tragically loses its head at the hands of Oddjob, is recorded at [Pinewood Studios|statue].",
+        },
+        {
+            // 00:32:20
+            title: "Departure",
+            description: "Goldfinger and Oddjob, as well as the Rolls Royce, leave from [Southend Airport|southend]. The scene with Bond [keeping an eye on the departing plane|southend_2] and [receiving information|southend_3] about the departure of his own plane - \"Mr. Bond. That's alright, I've got you booked out on the next flight to Geneva, leaving in half an hour\" is shot in a rather industrial area, some distance away from the terminal.",
+        },
+        {
+            // 00:33:05
+            title: "Switzerland",
+            description: "",
+        },
+        {
+            // 00:00:00
+            title: "",
+            description: "The first part of the car chase was filmed at [Black Park|black_park], right next to Pinewood Studios. The scenes at Goldfinger's factory were recorded at Pinewood studios, most notably along what is now called [Goldfinger Avenue|gf_avenue]",
+        },
+        {
+            // 00:00:00
+            title: "",
+            description: "",
+        },
+        {
+            // 00:00:00
+            title: "",
+            description: "",
+        },
+        {
+            // 00:00:00
+            title: "",
+            description: "",
+        },
+        {
+            // 00:00:00
+            title: "",
+            description: "",
+        },
+        {
+            // 00:00:00
+            title: "",
+            description: "",
+        },
+        {
+            // 00:00:00
+            title: "",
+            description: "",
+        },
+        {
+            // 00:00:00
+            title: "",
+            description: "",
+        },
+        {
+            // 00:00:00
+            title: "",
+            description: "",
+        },
+        {
+            // 00:00:00
+            title: "",
+            description: "",
+        },
+        {
+            // 00:00:00
+            title: "",
+            description: "",
+        },
+        {
+            // 00:00:00
+            title: "",
+            description: "",
+        },
+        {
+            // 00:00:00
+            title: "",
+            description: "",
+        },
+        {
+            // 00:00:00
+            title: "",
+            description: "",
+        },
+        {
+            // 00:00:00
+            title: "",
+            description: "",
+        },
+        {
+            // 00:00:00
+            title: "",
+            description: "",
+        },
+        {
+            // 00:00:00
+            title: "",
+            description: "",
+        },
+        {
+            // 00:00:00
+            title: "",
+            description: "",
+        },
+        {
+            // 00:00:00
+            title: "",
+            description: "",
+        },
+        {
+            title: "End credits",
+            description: "",
+        }
+    ],
+    locations: [
+        {
+            id: "stanwell",
+            name: "Stanwell",
+            position: {
+                lat: 51.449917,  
+                lng: -0.458436,
+            },
+        },
+        {
+            id: "fountainbleau",
+            name: "Fountainbleau Hotel",
+            position: {
+                lat: 25.817581,  
+                lng: -80.122636,
+            },
+            streetView: {
+                lat: 25.8177608,
+                lng: -80.1231309,
+                heading: 125,
+            },
+            mapView: {
+                lat: 25.817456, 
+                lng: -80.122000,
+                zoom: 19,
+            },
+            imageView: {
+                title: "Photo by Johan Åhlgren, 30 December 2013.",
+                href: "#",
+                alt: "Goldfinger - Fountainbleau Hotel",
+                src: "img/gf_hotel.jpg",
+            },
+        },
+        {
+            id: "hp",
+            name: "Houses of Parliament",
+            position: {
+                lat: 51.499272, 
+                lng: -0.124756,
+            },
+        },
+        {
+            id: "stoke",
+            name: "Stoke Park",
+            position: {
+                lat: 51.534547,  
+                lng: -0.602778,
+                zoom: 17,
+            },
+            streetView: {
+                lat: 51.5340652,
+                lng: -0.6024114,
+                heading: 100,
+            },
+        },
+        {
+            id: "golfshop",
+            name: "The Golf shop (demolished)",
+            position: {
+                lat: 51.534806,  
+                lng: -0.603071,
+                zoom: 17,
+            },
+        },
+        {
+            id: "hole18",
+            name: "The 18th hole",
+            position: {
+                lat: 51.533700,  
+                lng: -0.602071,
+                zoom: 17,
+            },
+        },
+        {
+            id: "statue",
+            name: "Statue at Pinewood Studios",
+            position: {
+                lat: 51.546417,   
+                lng: -0.533007,
+                zoom: 17,
+            },
+            streetView: {
+                lat: 51.5463528,   
+                lng: -0.5330983,
+                heading: 29.35,
+            }
+        },
+        {
+            id: "southend",
+            name: "Southend airport",
+            position: {
+                lat: 51.569923,    
+                lng: 0.692982,
+                zoom: 10,
+            },
+        },
+        {
+            id: "southend_2",
+            name: "Southend airport",
+            position: {
+                lat: 51.568112,     
+                lng: 0.699167,
+                zoom: 18,
+            },
+            streetView: {
+                lat: 51.568032,      
+                lng: 0.699236,
+            },
+        },
+        {
+            id: "southend_3",
+            name: "Southend airport - Bond gets information about his flight",
+            position: {
+                lat: 51.5679532,    
+                lng: 0.6992157,
+                zoom: 18,
+            },
+            streetView: {
+                lat: 51.5679532,    
+                lng: 0.6992157,
+                heading: 126,
+            }
+
+            // https://www.google.se/maps/@51.5679532,0.6992157,3a,75y,126.29h,92.22t/data=!3m6!1e1!3m4!1s8-hIEYwG68DpQxQb9uLkNQ!2e0!7i13312!8i6656
+        },
+
+        {
+            id: "gf_avenue",
+            name: "Goldfinger Avenue, Pinewood Studios",
+            position: {
+                lat: 51.548136,
+                lng: -0.536137,
+                zoom: 17,
+            },
+            streetView: {
+                lat: 51.5481292,
+                lng: -0.5361143,
+                heading: 285,
+                zzoom: 12,
+            },
+        },
+        
+        // Gas station "Aurora" in Switzerland: 46.632015, 8.592210
+        // https://www.20min.ch/schweiz/zentralschweiz/story/-James-Bond-Tankstelle--erlebt-ein-Comeback-16356541?fbclid=IwAR2v9lsB7j7MKiR0_FeTfnlqVcKhpWr7r5cFFixR8C5KRKq2vjBf0v_qnzQ
+        
+    ]
+}
+
