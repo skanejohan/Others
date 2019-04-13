889b3b7fa02b5c4daef7c17bce857a9825cd41ab
skanejohan <johan_m_ahlgren@hotmail.com>
2019-04-13 15:04:32 +0200

    Bond site: information added to Goldfinger

--- Web apps/bond/movies/1964_gf.js
+++ Web apps/bond/movies/1964_gf.js
@@ -45,104 +45,70 @@ const gf = {
         {
             // 00:33:05
             title: "Switzerland",
-            description: "",
+            description: "The scenes along the roads in Switzerland are shot along the Furkastrasse, between the small towns of Gletsch and Realp. In the opening shot we see Goldfinger's Rolls Royce pass [Hotel Belvedere|belvedere] close to the Rhone glacier which can be clearly seen in the background. Next, we see Bond and the Aston Martin leave the small village of [Realp|realp]. I believe that he leaves the village driving east, which does not make much sense, since the following scenes are shot west of the village. Although Bond's \"in-car navigation system\" indicates that Goldfinger is headed north from Geneva, we now see Goldfinger's Rolls Royce pass the [Hotel Galenstock|galenstock], about 300 kilometers away. In the following scene, as a white Mustang catches up with Bond, they pass another house. So far, I have not been able to figure out where this is. We see Tilly passing the [Hotel Belvedere|belvedere] before we get to the scene where [Goldfinger steps out of his car|apple] for a break and an apple.",
         },
         {
-            // 00:00:00
-            title: "",
-            description: "The first part of the car chase was filmed at [Black Park|black_park], right next to Pinewood Studios. The scenes at Goldfinger's factory were recorded at Pinewood studios, most notably along what is now called [Goldfinger Avenue|gf_avenue]",
+            // 00:34:40
+            title: "Tilly Soames",
+            description: "As [Bond watches him from above|watching], [Tilly fires a shot|tilly_shot] in the sand next to him. Bond then chases Tilly along the mountainous roads until all of a sudden, as Tilly overtakes Bond, they are on the long straight stretch of the [Furkastrasse|furka] heading east from Realp. This is also where Bond destroys Tilly's car. The garage where Bonds drops her off is the [Tankstelle Aurora|aurora], which was decommissioned in 2014.",
         },
         {
-            // 00:00:00
-            title: "",
-            description: "",
+            // 00:38:40
+            title: "Auric Enterprises",
+            description: "The exterior of Goldfinger's factory, \"Auric Enterprises\" is in reality the Pilatus Aircraft Factory, a good 60 to 70 kilometers from the Furka pass, where the rest of these scenes were shot. New buildings have been erected since, which makes it impossible to get the same view from the road that Bond did. The [old buildings|pilatus] are however still on the compound - the house with the jagged roof and the white-roofed building that held the \"Auric Enterprises\" logo.",
         },
         {
-            // 00:00:00
-            title: "",
-            description: "",
+            // 00:39:00
+            title: "Sneaking up on Mr. Goldfinger",
+            description: "From his position in the forest [above the factory|auric_above], Bond awaits darkness before descending into the compound. As soon as Bond reaches the factory, all scenes are recorded at Pinewood Studios. The [first scene|first] is shot along what is today called Goldfinger Avenue. Back in the forest, Bond runs into Tilly Masterson again.",
         },
         {
-            // 00:00:00
-            title: "",
-            description: "",
+            // 00:42:30
+            title: "Car chase",
+            description: "The first part of the car chase was filmed at [Black Park|black_park], right next to Pinewood Studios. After Oddjob has killed Tilly, Bond is apprehended but manages to throw his guard out of the car using the ejector seat, whereby the chase continues. The scenes at Goldfinger's factory were again recorded at Pinewood studios, most notably along [Goldfinger Avenue|gf_avenue]. Bond manages to escape death at Goldfinger's laser table only to be shot with a tranquilizer gun, after which he wakes up in...",
         },
         {
-            // 00:00:00
-            title: "",
-            description: "",
+            // 00:52:00
+            title: "Mr. Goldfinger's Lockheed JetStar, heading for Baltimore",
+            description: "All scenes from inside Glodfinger's plane are shot in the studio. One interesting fact: in all exterior scenes where we see Goldfinger' plane, it is flying right to left. The plane is a scale model, painted differently on the two sides. Later on in the film when we see the president's plane, it is for obvious reasons flying in the other direction.",
         },
         {
-            // 00:00:00
-            title: "",
-            description: "",
+            // 00:57:15
+            title: "\"Washington, Sir, on the green scrambler\"",
+            description: "Leiter calls M to inform him that Bond has landed at Friendship Airport, Baltimore. Their final destination is stated to be Bluegrass Field, Kentucky. Neither of these airports weher actually used in the film. Friendship Airport was renamed [Baltimore Washington International|TODO] in 1973. [Bluegrass|bluegrass] Airport is located just outside Lexington in Kentucky.",
         },
         {
-            // 00:00:00
-            title: "",
-            description: "",
+            // 00:57:50
+            title: "Arriving in Kentucky",
+            description: "The scenes at the airport are shot at the [RAF Northolt Airport|TODO], conveniently located just a short drive from Pinewood Studios. ",
         },
         {
-            // 00:00:00
-            title: "",
-            description: "",
+            // 01:00:25
+            title: "Auric Stud",
+            description: "The scenes at Goldfinger's farm were all shot at Pinewood Studios. We also see Felix Leiter and his partner in their car, close by. This scene, next to a building stating \"Kentucky Fried Chicken - Col. Sanders' Recipe\" was shot in Miami. The shopping mall located at [that place|kfc] today, still (2019) has a KFC. On the map on Goldfinger's wall, we clearly see [Fort Knox|knox]. A bit later, after Bond - in what may be the silliest scene in the whole franchise - has escaped and overhears the plan, Goldfinger mentions the intersection between Bullion Boulevard and Gold Vault Road. The street view image of [Fort Knox|knox] is taken from that intersection.",
         },
         {
-            // 00:00:00
-            title: "",
-            description: "",
+            // 01:13:10
+            title: "A pressing engagement",
+            description: "When Oddjob drives mr. Solo to his death, and the two CIA agents follow them from [their position|kfc], we see them move south on 7th Avenue. In the next scene, Oddjob drives north on 7th Avenue and takes [a left|royal_castle] onto 125th Street. The Royal Castle restaurant that he passes is today (2019) called \"Miami Finga Licking\". The two CIA agents then appear from the east, driving straight through the intersection to follow them. We then see them pass the J.D.Ball Ford dealership. In the same [place|ford], Ford is still selling cars. In the end, we end up at the [Atlantic Iron & Metal Company|atlantic] where Mr. Solo has a pressing engagement.",
         },
         {
-            // 00:00:00
-            title: "",
-            description: "",
+            // 01:17:30
+            title: "Back at the farm",
+            description: "Back at Goldfinger's farm, the complete plot is revealed, and Bond manages to swing Ms. Galore.",
         },
         {
-            // 00:00:00
-            title: "",
-            description: "",
+            // 01:24:20
+            title: "The raid on Fort Knox",
+            description: "The scenes at the airfield are, again, shot at [RAF Northolt Airport|TODO]. For the actual raid, the planes do actually fly over [Fort Knox|knox], at an altitude much lower than the one previously agreed upon. All outdoor scenes around Fort Knox were built at Pinewood Studios, particularly on the [\"Cobbled Street\" area|cobbled]. The scenes inside Fort Knox - Bond and Oddjob are locked inside, Bond and Oddjob fight, Bond electrocutes Oddjob, the bomb is stopped - are all classic, but obviously recorded in the studio.",
         },
         {
-            // 00:00:00
-            title: "",
-            description: "",
+            // 01:41:15
+            title: "Finale",
+            description: "Bond enters the plane to go meet the President, but encounters Goldfinger one last time. Again, [RAF Northolt Airport|TODO] was used for the airport scenes, while the scenes inside the plane were shot in the studio. The scenes back on the ground were shot [near the small lake|lake] in the garden behind Pinewood's administration building.",
         },
         {
-            // 00:00:00
-            title: "",
-            description: "",
-        },
-        {
-            // 00:00:00
-            title: "",
-            description: "",
-        },
-        {
-            // 00:00:00
-            title: "",
-            description: "",
-        },
-        {
-            // 00:00:00
-            title: "",
-            description: "",
-        },
-        {
-            // 00:00:00
-            title: "",
-            description: "",
-        },
-        {
-            // 00:00:00
-            title: "",
-            description: "",
-        },
-        {
-            // 00:00:00
-            title: "",
-            description: "",
-        },
-        {
+            // 01:44:30
             title: "End credits",
             description: "",
         }
@@ -269,10 +235,127 @@ const gf = {
                 lng: 0.6992157,
                 heading: 126,
             }
-
-            // https://www.google.se/maps/@51.5679532,0.6992157,3a,75y,126.29h,92.22t/data=!3m6!1e1!3m4!1s8-hIEYwG68DpQxQb9uLkNQ!2e0!7i13312!8i6656
         },
+        {
+            id: "belvedere",
+            name: "Hotel Belvedere - Goldfinger's Rolls Royce passes here on the way to Auric Enterprises",
+            position: {
+                lat: 46.576636,     
+                lng: 8.388404,
+                zoom: 18,
+            },
+            streetView: {
+                lat: 46.5756965,    
+                lng: 8.3901117,
+                heading: 220,
+            }
+        },
+        {
+            id: "realp",
+            name: "Realp village",
+            position: {
+                lat: 46.599698,      
+                lng: 8.503371,
+            },
+        },
+        {
+            id: "galenstock",
+            name: "Hotel Galenstock",
+            position: {
+                lat: 46.589667,      
+                lng: 8.479977,
+            },
+        },
+        {
+            id: "apple",
+            name: "Goldfinger steps out of his car for an apple",
+            position: {
+                lat: 46.590719,      
+                lng: 8.494690,
+            },
+        },
+        {
+            id: "watching",
+            name: "Bond watches Goldfinger from above",
+            position: {
+                lat: 46.590451,      
+                lng: 8.491816,
+            },
+        },
+        {
+            id: "tilly_shot",
+            name: "Tilly fires a shot in the road next to Bond",
+            position: {
+                lat: 46.590365,       
+                lng: 8.490986,
+            },
+        },
+        {
+            id: "furka",
+            name: "Furkastrasse - Bond destroys Tilly's car",
+            position: {
+                lat: 46.603262,      
+                lng: 8.516682,
+            },
+            streetView: {
+                lat: 46.6032873,
+                lng: 8.5167748,
+                heading: 248,
+            }
+        },
+        {
+            id: "aurora",
+            name: "Tankstelle Aurora - Bond drops Tilly off",
+            position: {
+                lat: 46.632201,       
+                lng: 8.592625,
+            },
+            streetView: {
+                lat: 46.6323566,
+                lng: 8.5929506,
+                heading: 230,
+            }
+        },
+        {
+            id: "pilatus",
+            name: "Auric Enterprises, exterior",
+            position: {
+                lat: 46.974422,       
+                lng: 8.381307,
+            },
+            streetView: {
+                lat: 46.9723675,
+                lng: 8.3815781,
+            },
 
+            mapView: {
+                lat: 46.974698, 
+                lng: 8.379921,
+                zoom: 18,
+            }
+        },
+        {
+            id: "auric_above",
+            name: "Auric Enterprises from above",
+            position: {
+                lat: 46.977683,        
+                lng: 8.381230,
+            },
+        },
+        {
+            id: "first",
+            name: "At Goldfinger's factory",
+            position: {
+                lat: 51.548152,       
+                lng: -0.53581,
+            },
+            streetView: {
+                lat: 51.548152,
+                lng: -0.53581,
+                heading: 300,
+            },
+        },
+        // TODO black_park
         {
             id: "gf_avenue",
             name: "Goldfinger Avenue, Pinewood Studios",
@@ -288,10 +371,104 @@ const gf = {
                 zzoom: 12,
             },
         },
-        
-        // Gas station "Aurora" in Switzerland: 46.632015, 8.592210
-        // https://www.20min.ch/schweiz/zentralschweiz/story/-James-Bond-Tankstelle--erlebt-ein-Comeback-16356541?fbclid=IwAR2v9lsB7j7MKiR0_FeTfnlqVcKhpWr7r5cFFixR8C5KRKq2vjBf0v_qnzQ
-        
+        {
+            id: "bluegrass",
+            name: "Bluegrass airport, Lexington, Kentucky",
+            position: {
+                lat: 38.034972,       
+                lng: -84.602829,
+            },
+        },
+        {
+            id: "kfc",
+            name: "The two CIA agents waiting in their car",
+            position: {
+                lat: 25.883984,        
+                lng: -80.210922,
+            },
+            streetView: {
+                lat: 25.8839749,
+                lng: -80.2109,
+                heading: 0,
+            },
+        },
+        {
+            id: "knox",
+            name: "Fort Knox",
+            position: {
+                lat: 37.883219,        
+                lng: -85.965304,
+            },
+            streetView: {
+                lat: 37.8823954,
+                lng: -85.9622588,
+                heading: 292,
+            },
+        },
+        {
+            id: "royal_castle",
+            name: "Oddjob takes a left turn with Mr. Solo in the back seat",
+            position: {
+                lat: 25.889179,        
+                lng: -80.210857,
+            },
+            streetView: {
+                lat: 25.8891401,
+                lng: -80.2108359,
+                heading: 273,
+            },
+        },
+        {
+            id: "ford",
+            name: "CIA agents pass the \"J.D. Ball\" car dealership.",
+            position: {
+                lat: 25.857646,        
+                lng: -80.210038,
+            },
+            streetView: {
+                lat: 25.857422,
+                lng: -80.2096214,
+                heading: 339,
+            },
+        },
+        {
+            id: "atlantic",
+            name: "Mr. Solo has a pressing engagement",
+            position: {
+                lat: 25.799439,         
+                lng: -80.248911,
+            },
+            streetView: {
+                lat: 25.7998467,
+                lng: -80.2484539,
+                heading: 223,
+            },
+        },
+        {
+            id: "cobbled",
+            name: "Fort Knox set at Pinewood",
+            position: {
+                lat: 51.551547,          
+                lng: -0.535311,
+            },
+        },
+        {
+            id: "lake",
+            name: "Bond and Pussy hide under the parachute",
+            position: {
+                lat: 51.545228,           
+                lng: -0.534239,
+            },
+        },
+        {
+            id: "TODO",
+            name: "Fort Knox",
+            position: {
+                lat: 37.883219,        
+                lng: -85.965304,
+            },
+
+        },
     ]
 }
 
