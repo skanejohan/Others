2b671d96d37b25f68e1c81791ca27a7c79982b70
skanejohan <johan_m_ahlgren@hotmail.com>
2023-10-30 07:46:28 +0100

    Add more details to the kitchen

--- Web apps/parswick_books/src/loc-kitchen.js
+++ Web apps/parswick_books/src/loc-kitchen.js
@@ -5,7 +5,13 @@ var kitchen = {
     initialize() {
         this.objects = [
             this._closedCupboard, 
-            this._sink 
+            this._tap,
+            this._waterCooker,
+            this._sink,
+            this._chair,
+            this._broomCupboard,
+            this._table,
+            this._fridge
         ];
         this.exits = [
             { rect: { left: 343, top: 83, right: 365, bottom: 278 }, leadsTo: fictionSection },
@@ -25,7 +31,7 @@ var kitchen = {
             if (o == this._cup) {
                 addObject(o, gameContext.inventory);
                 replaceObject(o, this._key, this.objects);
-                gameContext.message = "When you take the cup, a key appears.";
+                gameContext.message = "When you take the cup, a small key becomes visible.";
                 gameContext.messageRemainingMs = 2000;
             }
             if (o == this._key) {
@@ -48,34 +54,58 @@ var kitchen = {
     },
 
     _closedCupboard:             {
-        rect: { left: 365, top: 80, right: 445, bottom: 210 }, image: loadImage("closed-cupboard"), description: [ 
-            "It is a plain white cupboard, typical for a kitchen", 
-            "that has been decorated with economy in mind." ]
+        rect: { left: 480, top: 130, right: 580, bottom: 190 }, image: loadImage("closed-cupboard"), description: [ 
+            "A plain white cupboard, typical for a kitchen that", 
+            "has been decorated with economy in mind." ]
     },
 
     _openCupboard:             {
-        rect: { left: 345, top: 80, right: 445, bottom: 210 }, image: loadImage("open-cupboard"), description: [ 
-            "It is a plain white cupboard, typical for a kitchen", 
-            "that has been decorated with economy in mind. It is",
-            "also open." ]
+        rect: { left: 480, top: 130, right: 580, bottom: 190 }, image: loadImage("open-cupboard"), description: [ 
+            "The cupboard is open. And almost empty." ]
     },
 
     _sink:             {
-        rect: { left: 345, top: 220, right: 445, bottom: 380 }, description: [ 
+        rect: { left: 564, top: 250, right: 664, bottom: 410 }, description: [ 
             "The stainless steel sink has a strange coloration at", 
             "one end. You have a faint memory of pouring some kind", 
-            "of paint into it at an early age. The tap provides cold", 
-            "water only. On the sink there is a small water cooker." ]
+            "of paint into it at an early age." ]
     },
 
-    // "broomCupboard":  { x: 55, y: 55, w: 140, h: 40 },
+    _tap:             {
+        rect: { left: 624, top: 350, right: 664, bottom: 390 }, description: [ 
+            "The tap provides cold water only." ]
+    },
 
-    // "chair":  { x: 210, y: 180, w: 40, h: 40 },
+    _waterCooker:             {
+        rect: { left: 605, top: 260, right: 645, bottom: 300 }, description: [ 
+            "A small water cooker, mysteriously stained." ]
+    },
 
-    // "table":  { x: 100, y: 150, w: 100, h: 100 },
+    _chair:             {
+        rect: { left: 200, top: 320, right: 280, bottom: 400 }, description: [ 
+            "The chair looks uncomfortable and, having used it on", 
+            "many occasions, you know it is." ]
+    },
 
-    // "fridge":  { x: 55, y: 318, w: 80, h: 80 },
+    _table:             {
+        rect: { left: 64, top: 330, right: 184, bottom: 450 }, description: [ 
+            "This table's worn surface is covered by the doodles", 
+            "that you placed there in your childhood, while spending", 
+            "many long hours waiting for your parents to finish their", 
+            "business." ]
+    },
 
-    // "waterCooker": { x: 370, y: 230, w: 55, h: 55, layerIndex: 2 },
+    _broomCupboard:             {
+        rect: { left: 64, top: 130, right: 184, bottom: 290 }, description: [ 
+            "A plain white door. Behind it, you know you will find a", 
+            "lot of cleaning utensils. Nothing that feels remotely", 
+            "interesting." ]
+    },
 
+    _fridge:             {
+        rect: { left: 200, top: 200, right: 270, bottom: 290 }, description: [ 
+            "The white fridge is humming slightly. Once in a while it",
+            "makes a strange coughing noise. You take a peek inside.", 
+            "It is empty, as expected." ]
+    },
 }
