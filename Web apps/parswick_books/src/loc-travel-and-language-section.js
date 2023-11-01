7a3ee2ccab6444382b3e4d52a6d8a2f8337cf11b
skanejohan <johan_m_ahlgren@hotmail.com>
2023-11-01 17:01:46 +0100

    Latin dictionary introduced

--- Web apps/parswick_books/src/loc-travel-and-language-section.js
+++ Web apps/parswick_books/src/loc-travel-and-language-section.js
@@ -4,23 +4,13 @@ var travelAndLanguageSection = {
 
     initialize() {
         this.objects = [
+            this._dictionaryShelf1,
+            this._dictionaryShelf2,
             {
                 rect: { left: 100, top: 300, width: 120, height: 120 }, description: [ 
                     "Many a time have you sat in this worn armchair,", 
                     "dreaming of faraway beaches and cities where the", 
                     "lights never go out." ]
-            },
-            {
-                rect: { left: 80, top: 80, width: 120, height: 210 }, description: [ 
-                    "The shelf is filled with dictionaries and grammar", 
-                    "guides. To be honest, you have probably not opened", 
-                    "one of them after placing them in the shelf." ]
-            },
-            {
-                rect: { left: 220, top: 80, width: 120, height: 210 }, description: [ 
-                    "The shelf is filled with dictionaries and grammar", 
-                    "guides. To be honest, you have probably not opened", 
-                    "one of them after placing them in the shelf." ]
             },
             {
                 rect: { left: 520, top: 80, width: 120, height: 210 }, description: [ 
@@ -42,6 +32,40 @@ var travelAndLanguageSection = {
         ]
     },
 
-    update() {
-    }
+    update(mouseClickedAt) {
+        if (mouseClickedAt) {
+            var o = getObjectAt(mouseClickedAt, this.objects);
+            if ((o == this._dictionaryShelf1 || o == this._dictionaryShelf2) && gameContext.activeItem == office._unknownBook) {
+                addObject(this._latinDictionary, gameContext.inventory);
+                gameContext.message = [ 
+                    "Pondering the old book in your hand, you look more", 
+                    "closely for a latin dictionary which you are able to", 
+                    "find, squeezed between Astrid Stedje's \"Deutsche", 
+                    "Sprache gestern und heute\" and an old edition of", 
+                    "\"The Oxford Companion to English Literature\"." ];
+                gameContext.messageRemainingMs = 10000;
+            }
+        }
+    },
+
+    _dictionaryShelf1: {
+        rect: { left: 80, top: 80, width: 120, height: 210 }, description: [ 
+            "The shelf is filled with dictionaries and grammar", 
+            "guides. To be honest, you have probably not opened", 
+            "one of them after placing them in the shelf." ]
+    },
+
+    _dictionaryShelf2: {
+        rect: { left: 220, top: 80, width: 120, height: 210 }, description: [ 
+            "The shelf is filled with dictionaries and grammar", 
+            "guides. To be honest, you have probably not opened", 
+            "one of them after placing them in the shelf." ]
+    },
+
+    _latinDictionary: {
+        rect: { left: 0, top: 0, width: 0, height: 0 }, image: loadImage("latin-dictionary"), description: [ 
+            "The cover of this book simply says \"A Latin Dictionary\".", 
+            "\"Well\", you mutter to yourself, \"I guess no more", 
+            "information is needed\"." ]
+    },
 }
