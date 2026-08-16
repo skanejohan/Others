74ac6ab30dffbfdcca479e47350c03b26ea45281
skanejohan <johan_m_ahlgren@hotmail.com>
2026-08-16 20:44:13 +0200

    js13k 2026 - 1

---
+++ Web apps/js13k2026/main.js
@@ -0,0 +1,25 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+var lastTime = Date.now();
+let gameLoop = () => {
+    let now = Date.now();
+    let dt = (now - lastTime);
+    // update(level, dt);
+    // render(level, canvas.width, canvas.height);
+    requestAnimationFrame(gameLoop);
+    lastTime = now;
+}
+
+setTimeout(() => {
+    gameLoop();
+}, 100);
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+window.addEventListener('resize', resizeCanvas, false);
+
+resizeCanvas();
