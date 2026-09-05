c6900b1887f0c0f2f7e7c62f50a4db82bc0e2bb4
skanejohan <johan_m_ahlgren@hotmail.com>
2026-09-05 10:55:19 +0200

    HUD scales correctly

--- Web apps/js13k2026/render.js
+++ Web apps/js13k2026/render.js
@@ -130,60 +130,63 @@ let _renderInformation = () => {
         return `rgb(${r}, ${g}, ${b})`;
     }
 
-    let infoCanvas = _createCanvas(ww(51), hh(20), _ctx => {
+    let s = x => x * _cell;
+
+    let infoCanvas = _createCanvas(s(53), s(20), _ctx => {
 
         let renderGauge = (x, y, level, invertColor) => {
-            let cx = xx(x);
-            let cy = yy(y);
-            let r = ww(10);
+            let cx = x * _cell;
+            let cy = y * _cell;
+            let r = 10 * _cell;
 
             _ctx.fillStyle = "#210413";
             _ctx.beginPath();
-            _ctx.arc(cx, cy, r, 0, 2 * Math.PI);
+            _ctx.arc(s(x), s(y), s(10), 0, 2 * Math.PI);
             _ctx.fill();
 
             level = Math.min(Math.max(level, 0), 1); // Clamp level between 0 and 1
             let angle = 0.75 + level * 1.5;
             _ctx.fillStyle = getPercentColor(invertColor ? 1 - level : level);
             _ctx.beginPath();
-            _ctx.arc(cx, cy, r, 0.75 * Math.PI, angle * Math.PI);
-            _ctx.lineTo(cx, cy);
+            _ctx.arc(s(x), s(y), s(10), 0.75 * Math.PI, angle * Math.PI);
+            _ctx.lineTo(s(x), s(y));
             _ctx.fill();
 
             _ctx.fillStyle = "#0d0107";
             _ctx.beginPath();
-            _ctx.arc(cx, cy, r * 0.7, 0, 2 * Math.PI);
+            _ctx.arc(s(x), s(y), s(7), 0, 2 * Math.PI);
             _ctx.fill();
 
             _ctx.fillStyle = "white";
             _ctx.beginPath();
-            _ctx.arc(cx - r * 0.5, cy + r * 0.5, 3, 0, 2 * Math.PI);
+            _ctx.arc(s(x) - s(5), s(y) + s(5), s(0.5), 0, 2 * Math.PI);
             _ctx.fill();
             _ctx.beginPath();
-            _ctx.arc(cx, cy - r * 0.7 , 3, 0, 2 * Math.PI);
+            _ctx.arc(s(x), s(y) - s(7), s(0.5), 0, 2 * Math.PI);
             _ctx.fill();
             _ctx.beginPath();
-            _ctx.arc(cx + r * 0.5, cy + r * 0.5, 3, 0, 2 * Math.PI);
+            _ctx.arc(s(x) + s(5), s(y) + s(5), s(0.5), 0, 2 * Math.PI);
             _ctx.fill();
         }
 
         // Speed
-        renderGauge(-7, 10, speed / 3, true);
+        renderGauge(10, 10, speed / 3, true);
 
         // Distance
-        renderGauge(23, 10, distance / trackDistance, false);
+        renderGauge(42, 10, distance / trackDistance, false);
 
         _ctx.fillStyle = getPercentColor(energy / 100);
         _ctx.strokeStyle = "black";
         _ctx.lineWidth = 2;
         let h = energy * 15 / 100;
-        _ctx.fillRect(xx(4), yy(15-h), ww(8), hh(h));
-        _ctx.strokeRect(xx(4), yy(0), ww(8), hh(15));
+        _ctx.fillRect(s(22), s(15-h), s(8), s(h));
+        _ctx.strokeRect(s(22), 0, s(8), s(15));
 
         // Level
         _ctx.font = "48px Arial";
-        _ctx.fillStyle = "black";
-        _ctx.fillText(`${level}`, xx(7), yy(19));
+        _ctx.fillStyle = "white";
+        _ctx.textAlign = "center";
+        _ctx.fillText(`${level}`, s(26), s(19));
     });
 
     ctx.globalAlpha = 0.5;
